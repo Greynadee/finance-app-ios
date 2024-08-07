f48b4dce9d072c284cbd915a5485c6b1367e8675
Siddharth Joshi <siddharthjoshi4568@gmail.com>
2024-08-07 15:04:59 +0530

    ui pushed

---
+++ Shared/Finance_App_Dark_ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_App_Dark_ThemeApp.swift
+//  Shared
+//
+//  Created by Pawan Pareek on 8/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_App_Dark_ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            YourBalanceView()
+        }
+    }
+}
